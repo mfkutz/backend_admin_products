f0598ee1bfec40af456109322dc3fdc6622174ff
Martin <mfkutz@gmail.com>
2024-11-09 19:28:26 -0300

    modifing cors

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ connectDB();
 const server = express();
 
 //Permitir conexiones
-const corsOptions: CorsOptions = {
+/* const corsOptions: CorsOptions = {
   origin: function (origin, callback) {
     // console.log(origin);
     if (origin === process.env.FRONTEND_URL) {
@@ -35,6 +35,11 @@ const corsOptions: CorsOptions = {
       callback(new Error("CORS Error"));
     }
   },
+}; */
+
+const corsOptions: CorsOptions = {
+  origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+  credentials: true, // Habilita el envío de cookies
 };
 
 server.use(cors(corsOptions));
